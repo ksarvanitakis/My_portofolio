f5eec3963b41c7e18aad769571e891086d240185
Konstantinos Arvanitakis <74551245+ksarvanitakis@users.noreply.github.com>
2022-06-16 22:07:46 +0200

    experience component updated

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import './about.css'
-
-
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.JPG";
+import { FaAward } from "react-icons/fa";
+import { FaUniversity } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 function About() {
   return (
-    <section id='about'>About</section>
-  )
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small> 3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FaUniversity className="about__icon" />
+              <h5>Background</h5>
+              <small> Studies </small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small> 2 Projects </small>
+            </article>
+          </div>
+
+          <p>
+            A former Environmental scientist, decided to do a career change and
+            follow my passion for web development. I am a goal-oriented and
+            cheerful team player always eager to accept new challenges. Strong
+            supporter of lifelong learning that keeps being my motivation in
+            life.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
--- src/components/exprerience/Experience.js
+++ src/components/exprerience/Experience.js
@@ -1,11 +1,100 @@
-import React from 'react'
-import './experience.css'
-
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
+import { FaHtml5 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { RiReactjsFill } from "react-icons/ri";
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
-}
+    <section id="experience">
+      <h5>Skills that I have</h5>
+      <h2>My experience</h2>
 
-export default Experience
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaHtml5 className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoLogoJavascript className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <RiReactjsFill className="experience__details-icon" />
+              <div>
+                <h4>TypeScript</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NodeJS</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaHtml5 className="experience__details-icon" />
+              <div>
+                <h4>ExpressJS</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoLogoJavascript className="experience__details-icon" />
+              <div>
+                <h4>MongoDB/PostgreSQL</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <RiReactjsFill className="experience__details-icon" />
+              <div>
+                <h4>REST</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>GraphQL</h4>
+                {/* <small className="text-light">Experienced</small> */}
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
